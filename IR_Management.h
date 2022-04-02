822db25cea00fc9e83c529b25544290db82f1c63
tgriozel <thomas.griozel@gmail.com>
2022-04-02 16:45:02 +0200

    Modularize

---
+++ IR_Management.h
@@ -0,0 +1,8 @@
+#ifndef IR_MANAGEMENT_H
+#define IR_MANAGEMENT_H
+
+void ir_setup();
+
+void ir_loop();
+
+#endif
--- Main.ino
+++ Main.ino
@@ -1,49 +1,9 @@
-#include <Arduino.h>
-
-#define DECODE_NEC
-#define DECODE_DISTANCE
-
-#define IR_RECEIVE_PIN 2
-#define IR_SEND_PIN 3
-
-#include <IRremote.hpp>
+#include "IR_Management.h"
 
 void setup() {
-    Serial.begin(115200);
-    delay(4000);
-
-    IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
-    IrSender.begin(IR_SEND_PIN);
-}
-
-void send_ir_data() {
-    uint16_t sAddress = 0x0102;
-    uint8_t sCommand = 0x34;
-    uint8_t sRepeats = 1;
-    IrSender.sendNEC(sAddress, sCommand, sRepeats);
-}
-
-void receive_ir_data() {
-    if (IrReceiver.decode()) {
-        IRData data = IrReceiver.decodedIRData;
-        Serial.print(F("Decoded protocol: "));
-        Serial.print(getProtocolString(data.protocol));
-        Serial.print(F("Decoded raw data: "));
-        Serial.print(data.decodedRawData, HEX);
-        Serial.print(F(", decoded address: "));
-        Serial.print(data.address, HEX);
-        Serial.print(F(", decoded command: "));
-        Serial.println(data.command, HEX);
-        IrReceiver.resume();
-    }
+    ir_setup();
 }
 
 void loop() {
-    send_ir_data();
-
-    // wait for the receiver state machine to detect the end of a protocol
-    delay((RECORD_GAP_MICROS / 1000) + 5);
-    receive_ir_data();
-
-    delay(100);
+    ir_loop();
 }
